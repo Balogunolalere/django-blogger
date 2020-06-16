6e89a9252c29b0d35c3cf5ecb3c5bbffeb4ef1ed
Balogun_jamal <lordareello@gmail.com>
2020-06-16 11:21:03 +0100

    another commit for heroku

--- blogx/settings.py
+++ blogx/settings.py
@@ -16,8 +16,7 @@ import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# Activate Django-Heroku.
-django_heroku.settings(locals())
+
 
 
 
